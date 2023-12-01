2b00ae4cfe975397a2a76fd08e4bca15fec772d9
leejaeseong11 <easeon78@gmail.com>
2023-12-01 12:31:38 +0900

    feat: chat test

--- src/main.js
+++ src/main.js
@@ -7,3 +7,5 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+app.config.globalProperties.backURL = 'http://192.168.1.58:8080/codesparring'
+app.config.globalProperties.socketURL = 'ws://192.168.1.58:8080/codesparring/ws/session'
