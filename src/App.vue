ac5bf99f324bd90d3ec0cfbc05992a1fcfbc2c74
qbobl5 <ddddd991125@gmail.com>
2023-12-04 01:53:28 +0900

    feat-quizAdd : vue test

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,23 @@
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
+      &nbsp;&nbsp;&nbsp; <!--문제 추가하기 임시 버튼 생성해놓은거임-->
+      <RouterLink to="/quiz/add">
+        <button class="addQuizBt">문제 추가하기</button>
+      </RouterLink>
     </nav>
-  </header>
+  </header> 
   <RouterView />
 </template>
 
-<style></style>
+<style>
+/* 임시 생성 */
+.addQuizBt{
+  width: 120px; 
+  height: 45px; 
+  background-color: #354c7c; 
+  color: #eaebfe;
+  border: 3px solid #354c7c;  
+  border-radius: 5px;
+}
+</style>
---
+++ src/views/quiz/AddQuiz.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>문제 추가하기 화면</div>
+</template>
+<script>
+export default{
+    name: 'addQuiz',
+}
+</script>
+<style>
+</style>
