186de7080b3b231ef78bfd66a64cfb23324ca2dd
qbobl5 <ddddd991125@gmail.com>
2023-12-07 14:25:28 +0900

    feat-admin: AdminQuizPage component 에 추가

--- src/views/admin/adminPage.vue
+++ src/views/admin/adminPage.vue
@@ -2,13 +2,18 @@
     <main id="main-container"></main>
 </template>
 <script>
+// import AdminQuizPage from '../../components/admin/adminQuizPage.vue'
 export default {
     name: 'AdminPage',
+    // components: {AdminQuizPage},
     data() {
         return {
 
         }
-    }
+    },
+    methods: {
+
+    },
 }
 </script>
 <style scoped>
