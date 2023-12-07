d19cf5458880b4ee376c0fbe53bd82df3b46c0b2
qbobl5 <ddddd991125@gmail.com>
2023-12-07 14:15:04 +0900

    feat-admin: adminPage.vue 파일 생성

---
+++ src/views/admin/adminPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <main id="main-container"></main>
+</template>
+<script>
+export default {
+    name: 'AdminPage',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
