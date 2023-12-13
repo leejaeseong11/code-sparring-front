81511faaf3f69808a02a9cd46a8d905b14b1f57f
qbobl5 <ddddd991125@gmail.com>
2023-12-13 16:37:29 +0900

    feat-codeView: vue 파일 생성

---
+++ src/views/code/CodeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>code-view</div>
+</template>
+<script>
+export default {
+    name: 'CodeView',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
