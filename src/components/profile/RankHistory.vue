9bbdd70026a14e7ebf92d666510d812c4217a88b
qbobl5 <ddddd991125@gmail.com>
2023-12-19 18:35:52 +0900

    feat-rankhistory: 랭크 전적 조회 오류 해결

--- src/components/profile/RankHistory.vue
+++ src/components/profile/RankHistory.vue
@@ -20,6 +20,7 @@
 
     <div id="rank-content" ref="scrollContainer" @scroll="handleScroll">
       <div v-for="rank in rankList" :key="rank" :class="'rank-obj-' + rank.gameResult">
+        <div v-if="rankList.length==1 && rank.gameResult==null">전적이 존재하지 않습니다.</div>
         <span class="opposing-name">대결 상대 : {{ rank.opposingName }}</span>
         <span class="game-result">{{ rank.gameResult }}</span>
       </div>
